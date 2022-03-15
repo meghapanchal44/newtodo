b94599253ec71f9ade31b88e1d0b5053784f29a5
MeghaPanchal <84782627+meghapanchal44@users.noreply.github.com>
2022-03-15 10:38:18 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('#myform').validate({
+        rules: {
+            email: {
+                required:true
+            },  
+            fname:{
+                required:true
+            },
+            lname:{
+                required:true
+            },
+            checkbox:{
+                required:true
+            },
+            messages: {
+                email: {
+                    required: "enter valid email"
+                }
+            }
+        }
+    })
+    $('#submit').click(function () {
+        $('#myform').submit();
+
+    });
+});
+function myFunction() {
+    var x = document.getElementById("pswd");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
